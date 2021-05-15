084e8f64ab731e01a6ad16efa87c6c1fe87e19f8
FairyTale <egorusdnepr@gmail.com>
2021-05-15 20:01:34 +0300

    modified essence Tournament

--- Hackaton_team3/Tournament.cs
+++ Hackaton_team3/Tournament.cs
@@ -10,6 +10,7 @@ namespace Hackaton_team3
     {
         private string _name;
         private string _description;
+        private string _location;
         public string Name
         {
             get
@@ -50,10 +51,51 @@ namespace Hackaton_team3
         public DateTime EndRegistration { get; set; }
         public Division Division { get; set; }
 
+        Dictionary<Participant, int> points;
+
+        public string Location
+        {
+            get { return _location; }
+            set
+            {
+                if (value==null)
+                {
+                    throw new ArgumentNullException();
+                }
+                else
+                {
+                    _location = value;
+                }           
+            }
+        }
+
+        public void AddPointsToTeam(Participant team, int p)
+        {
+            if (!points.ContainsKey(team))
+            {
+                points.Add(team, p);
+            }
+            else
+            {
+                points[team] = points[team] + p;
+            }
+        }
+
         public override bool Equals(object obj)
         {
             bool result = false;
-            
+            if (obj is Tournament)
+            {
+                Tournament temp = (Tournament)obj;
+                if (Name==temp.Name && Description == temp.Description && Start==temp.Start && EndRegistration == temp.EndRegistration && Division == temp.Division)
+                {
+                    result = true;
+                }
+                
+            }
+            return result;
+
+
         }
     }
 }
