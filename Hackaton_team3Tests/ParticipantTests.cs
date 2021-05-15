005e75180fd81866296ee08c6d8708c2143e7ff0
FairyTale <egorusdnepr@gmail.com>
2021-05-15 17:05:29 +0300

    added essence Participant

---
+++ Hackaton_team3Tests/ParticipantTests.cs
@@ -0,0 +1,46 @@
+﻿using Hackaton_team3;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackaton_team3Tests
+{
+    class ParticipantTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]//new Participant("Max", Division.Beginner), new Participant("Max", Division.Beginner), true
+        public void ConstructorTest()
+        {
+            Participant first = new Participant("Max", Division.Beginner);
+            Assert.AreEqual("Max", first.Name);
+            Assert.AreEqual(Division.Beginner, first.Division);
+            
+           
+        }
+
+        [Test]//new Participant("Max", Division.Beginner), new Participant("Max", Division.Beginner), true
+        public void EqualsTest1()
+        {
+            Participant first = new Participant("Max", Division.Beginner);
+            Participant second = new Participant("Max", Division.Beginner);
+            bool act = first.Equals(second);
+            Assert.AreEqual(true, act);
+        }
+
+        [Test]//new Participant("Max", Division.Beginner), new Participant("Max", Division.Beginner), true
+        public void EqualsTest2()
+        {
+            Participant first = new Participant("Max", Division.Beginner);
+            Participant second = new Participant("Payne", Division.Advanced);
+            bool act = first.Equals(second);
+            Assert.AreEqual(false, act);
+        }
+
+
+    }
+}
