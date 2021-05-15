d8f8a7bc5232333f5b3b50597e3d17c178708b24
Brovko Alexandra <brovko.sasha2002@gmail.com>
2021-05-15 17:07:19 +0300

    Created Layers

---
+++ Hackaton_team3/Layers.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackaton_team3
+{
+    public class Layers
+    {
+        private int _horisontal;
+        public int Horisontal
+        {
+            get
+            {
+                return _horisontal;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _horisontal = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        private int _vertical;
+        public int Vertical
+        {
+            get
+            {
+                return _horisontal;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _vertical = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect value");
+                }
+            }
+        }
+
+        public Layers(int width, int height)
+        {
+            Horisontal = width;
+            Vertical = height;
+        }
+
+        public override string ToString()
+        {
+
+            return $"{_horisontal}, {_vertical}";
+
+        }
+
+    }
+}
+
---
+++ Hackaton_team3Tests/LayersTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackaton_team3.Tests
+{
+    public class LayersTests
+    {
+        [TestCase(21, 78, "21, 78") ]
+        [TestCase(1, 999, "1, 999")]
+        public void ToString_WhenTestPassed_ReturnString(int actualHorisontal, int actualVertical,string expected)
+        {
+            Layers actualLayers = new Layers(actualHorisontal,actualVertical);
+            string actual = actualLayers.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(-10,-100)]
+        public void ToString_WhenTestNotPassed_ReturnArgumentOutOfRangeException(int actualHorisontal, int actualVertical)
+        {
+            Assert.Throws<ArgumentException>(() => new Layers(actualHorisontal, actualVertical));
+
+        }
+    }
+}
