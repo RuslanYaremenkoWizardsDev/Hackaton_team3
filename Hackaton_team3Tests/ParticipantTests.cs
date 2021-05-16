1baf7a31a99c05fa405ad7aad687f8e2acd54845
Rostislav <skochmen@gmail.com>
2021-05-16 12:38:42 +0300

    Refactoring done

--- Hackaton_team3Tests/ParticipantTests.cs
+++ Hackaton_team3Tests/ParticipantTests.cs
@@ -1,24 +1,19 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Hackaton_team3.Tests
 {
     class ParticipantTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [TestCase("1", Division.Advanced, "1", Division.Advanced)]
         [TestCase("2", Division.Middle, "2", Division.Middle)]
         [TestCase("3", Division.Beginner, "3", Division.Beginner)]
         public void ConstructorTest(string actualName, Division actualDivision, string expectedName, Division expectedDivision)
         {
-            Participant actualParticipant = new Participant(actualName, actualDivision);
+            Participant actualParticipant =  Participant.Create(actualName, actualDivision);
+
             Assert.AreEqual(expectedName, actualParticipant.Name);
             Assert.AreEqual(expectedDivision, actualParticipant.Division);
-            
-           
         }
 
         [TestCase("1", Division.Advanced, "1", Division.Advanced, true)]
@@ -26,14 +21,31 @@ namespace Hackaton_team3.Tests
         [TestCase("1", Division.Advanced, "1", Division.Middle, false)]
         public void Equals_WhenValidTestPassed_ShouldRerturnTrueOrFalse(string actualName, Division actualDivision, string expectedName, Division expectedDivision, bool expected)
         {
-            Participant actualParticipant = new Participant(actualName, actualDivision);
-            Participant expectedParticipant = new Participant(expectedName, expectedDivision);
+            Participant actualParticipant = Participant.Create(actualName, actualDivision);
+            Participant expectedParticipant = Participant.Create(expectedName, expectedDivision);
 
             bool actual = actualParticipant.Equals(expectedParticipant);
 
             Assert.AreEqual(expected, actual);
         }
 
+        [TestCase(null,Division.Advanced)]
+        [TestCase(null, Division.Beginner)]
+        [TestCase(null, Division.Middle)]
+        public void Create_WhenInvalidTestPassed_ShouldReturnArgumentNullException(string actualName,
+            Division actualDivision)
+        {
+            Assert.Throws<ArgumentNullException>(() => Participant.Create(actualName, actualDivision));
+        }
 
+        [TestCase(null, Division.Advanced)]
+        [TestCase(null, Division.Beginner)]
+        [TestCase(null, Division.Middle)]
+        public void SetName_WhenInvalidTestPassed_ShouldReturnArgumentNullException(string actualName,
+            Division actualDivision)
+        {
+            Participant actual = Participant.Create(string.Empty, actualDivision);
+            Assert.Throws<ArgumentNullException>(() =>actual.Name = actualName );
+        }
     }
 }
